25985bcb9630f1c20dddcc56065c8be624da80e8
Pūslys, A. (Adomas, Student B-TCS) <a.puslys@student.utwente.nl>
2025-02-20 13:05:22 +0100

    Add custom validation functions for UUID and password

---
+++ src/validations/custom.validation.js
@@ -0,0 +1,22 @@
+const uuid = (value, helpers) => {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    if (!uuidRegex.test(value)) {
+        return helpers.message('"{{#label}}" myst be valid UUID');
+    }
+    return value;
+};
+
+const password = (value, helpers) => {
+    if (value.length < 8) {
+        return helpers.message('password must be at least 8 chars');
+    }
+    if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+        return helpers.message('password must contain at least 1 letter and 1 number');
+    }
+    return value;
+};
+
+module.exports = {
+    uuid,
+    password,
+};
