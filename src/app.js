8f3b112881ca74a15ef7fdf08971d24da869e737
Pūslys, A. (Adomas, Student B-TCS) <a.puslys@student.utwente.nl>
2025-02-20 13:08:36 +0100

    Add global error handling in app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const routes = require('../src/routes/index.js');
-
 const app = express();
+const routes = require('./routes');
 
 app.use(express.json());
-
 app.use('/api', routes);
 
 app.use((req, res, next) => {
     res.status(404).send({error: 'Not Found'});
+});
+
+/**
+ * Global error handler
+ */
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(err.statusCode || 400).json({
+        message: err.message || 'Internal Server Error'
+    });
 });
 
 module.exports = app;
