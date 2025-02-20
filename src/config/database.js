b2c5ed5138771901cebff7875fb091f99ecf5be8
Pūslys, A. (Adomas, Student B-TCS) <a.puslys@student.utwente.nl>
2025-02-20 10:18:35 +0100

    Initial commit: setup project structure

---
+++ src/config/database.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize');
+
+const {
+    SQL_USERNAME,
+    SQL_PASSWORD,
+    SQL_DATABASE_NAME,
+    SQL_HOST,
+    SQL_PORT,
+    SQL_DIALECT,
+    SQL_SSL_REQUIRE,
+    SQL_SSL_REJECT_UNAUTHORIZED
+} = process.env
+
+const sequelize = new Sequelize(SQL_DATABASE_NAME, SQL_USERNAME, SQL_PASSWORD, {
+    host: SQL_HOST,
+    port: SQL_PORT,
+    dialect: SQL_DIALECT || 'postgres',
+    dialectOptions: {
+        ssl: {
+            required: SQL_SSL_REQUIRE === 'true',
+            rejectUnauthorized: SQL_SSL_REJECT_UNAUTHORIZED === 'true',
+        },
+    },
+    logging: false,
+});
+
+module.exports = sequelize;
