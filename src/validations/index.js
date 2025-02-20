1bdaa39112ebcd65df2962e21ae5b89af31a2ece
Pūslys, A. (Adomas, Student B-TCS) <a.puslys@student.utwente.nl>
2025-02-20 13:08:07 +0100

    Add validations index to aggregate and export validation schemas

---
+++ src/validations/index.js
@@ -0,0 +1,11 @@
+const userValidation = require('./user.validation');
+const eventValidation = require('./event.validation');
+const registrationValidation = require('./registration.validation');
+const customValidation = require('./custom.validation');
+
+module.exports = {
+    userValidation,
+    eventValidation,
+    registrationValidation,
+    customValidation,
+};
